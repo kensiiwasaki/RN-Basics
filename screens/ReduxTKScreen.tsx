55d001e24e711e9d8cfda5d809e16f271339c672
kensiiwasaki <kensiiwasaki@gmail.com>
2023-05-01 04:02:05 +0900

    modify: react-reduxの使用

--- screens/ReduxTKScreen.tsx
+++ screens/ReduxTKScreen.tsx
@@ -1,13 +1,27 @@
 import { View, Text } from 'react-native';
 import tw from 'tailwind-rn';
-import React, { FC, useEffect } from 'react';
+import React, { FC, useEffect, useState } from 'react';
+import { FontAwesome } from '@expo/vector-icons';
 import type { NativeStackScreenProps } from '@react-navigation/native-stack';
 import { RootStackParamList } from '../types/types';
-import { Button } from 'react-native-elements';
+import { Button, Input } from 'react-native-elements';
+import { useSelector, useDispatch } from 'react-redux';
+import {
+  increment,
+  decrement,
+  addByPayload,
+  selectCount,
+} from '../slices/counterSlice';
 
 type Props = NativeStackScreenProps<RootStackParamList, 'ReduxTK'>;
 
 export const ReduxTKScreen: FC<Props> = ({ navigation }) => {
+  const [amount, setAmount] = useState('0');
+  const count = useSelector(selectCount);
+  const dispatch = useDispatch();
+
+  const incrementAmount = Number(amount) || 0;
+
   useEffect(() => {
     console.log('mounted ReduxToolKit');
     return () => {
@@ -16,11 +30,32 @@ export const ReduxTKScreen: FC<Props> = ({ navigation }) => {
   }, []);
   return (
     <View style={tw('flex-1 bg-gray-300 justify-center items-center')}>
-      <Text>Redux ToolKit</Text>
+      <Text>Redux ToolKit {count}</Text>
       <View style={tw('my-3')}>
         <Button
           title="Go to FlexBox"
           onPress={() => navigation.navigate('FlexBox')}
+        />
+      </View>
+      <View style={tw('my-1')}>
+        <Button title="increment" onPress={() => dispatch(increment())} />
+      </View>
+      <View style={tw('my-1')}>
+        <Button title="decrement" onPress={() => dispatch(decrement())} />
+      </View>
+      <Input
+        placeholder="type add number"
+        leftIcon={<FontAwesome name="pencil" size={24} color="gray" />}
+        value={amount}
+        onChangeText={(txt: string) => setAmount(txt)}
+      />
+      <View style={tw('my-1')}>
+        <Button
+          title="increment by amount"
+          onPress={() => {
+            dispatch(addByPayload(incrementAmount));
+            setAmount('0');
+          }}
         />
       </View>
     </View>
