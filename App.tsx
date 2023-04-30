d2370f69f4c2917f75514c2c12aba8798032d6d8
kensiiwasaki <kensiiwasaki@gmail.com>
2023-05-01 04:01:31 +0900

    modify: Providerでラップ

--- App.tsx
+++ App.tsx
@@ -1,6 +1,12 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+import { store } from './store';
 import { RootNavigator } from './navigations/RootNavigator';
 
 export default function App() {
-  return <RootNavigator />;
+  return (
+    <Provider store={store}>
+      <RootNavigator />
+    </Provider>
+  );
 }
