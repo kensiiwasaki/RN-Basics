40fc0d86b8e2f5582c0bfad5ab83acd0ddf9586d
kensiiwasaki <kensiiwasaki@gmail.com>
2023-05-01 01:09:01 +0900

    modify: RootNavigationを使用

--- App.tsx
+++ App.tsx
@@ -1,12 +1,6 @@
-import { StatusBar } from 'expo-status-bar';
-import { Text, View } from 'react-native';
-import tw from 'tailwind-rn';
+import React from 'react';
+import { RootNavigator } from './navigations/RootNavigator';
 
 export default function App() {
-  return (
-    <View style={tw('flex-1 bg-gray-300 justify-center items-center')}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  return <RootNavigator />;
 }
